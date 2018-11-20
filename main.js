8241638d21c519bd151e1ec5f2017f33501136d6
Mindstormer <louisemileploix@icloud.com>
2018-11-20 11:23:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,11 @@ function fetchPage(path, callback) {
     xhr.send();
 }
 
-if (window.location.href.split('#')[1] != 'hide') {
+if (window.location.href.split('#')[1].indexOf('invert') != -1) {
+    document.body.style.filter = 'invert(1)';
+}
+
+if (window.location.href.split('#')[1].indexOf('hide') != -1) {
     const options = {
         'HTML (Taken from the <a href="https://google.com">google.com</a> home page)': 'google.html',
         'JavaScript (Taken from <a href="https://jquery.com/">jQuery</a>, a popular JS library)': 'jquery.js',
